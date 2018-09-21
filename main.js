4c6507ff1997e4a8190fbba0cc1a8f104512f102
Quyen Nguyen Thi <quyen.nguyen2@ntq-solution.com.vn>
2018-09-21 16:28:32 +0700

    first code

---
+++ main.js
@@ -0,0 +1,22 @@
+// create array secreat word
+var words = [
+    "javscript",
+    "hang",
+    "beautiful",
+    "payment",
+    "information",
+    "customer"
+];
+// select random word
+const word = words[Math.floor(Math.random() * words.length)];
+// create answer array
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+//remain letter
+var remainLetters = word.length;
+// loop
+while(remainLetters > 0) {
+
+}
