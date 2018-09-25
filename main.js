6f6636f681e85325a326dae14c44b677f652bc3d
Quyen Nguyen Thi <quyen.nguyen2@ntq-solution.com.vn>
2018-09-25 07:16:09 +0700

    complete hangman game

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-// create array secreat word
-var words = [
-    "javscript",
-    "hang",
-    "beautiful",
-    "payment",
-    "information",
-    "customer"
-];
-// select random word
-const word = words[Math.floor(Math.random() * words.length)];
-// create answer array
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-//remain letter
-var remainLetters = word.length;
-// loop
-while(remainLetters > 0) {
 
+// create array words
+var words = [
+    "middlemoon",
+    "moon",
+    "tree",
+    "bluetooth"
+];
+// select random word in words
+var answerWord = words[Math.floor(Math.random() * words.length)];
+// create answer _
+var arrayAnswer = [];
+for(let i = 0; i < answerWord.length; i++) {
+    arrayAnswer[i] = "_";
 }
+console.log(arrayAnswer);
+// create remain
+remainLetters = answerWord.length;
+// loop
+do {
+    alert(arrayAnswer.join(" "));
+    var guess = prompt('Input letter');
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert('Please enter 1 charactor');
+    } else {
+        for(let j = 0; j < answerWord.length; j++) {
+            if (answerWord[j] === guess) {
+                arrayAnswer[j] = guess;
+                remainLetters--;
+            }
+        }
+    }
+} while (remainLetters > 0)
+alert('Good job! Answer is ' + arrayAnswer.join(''));
